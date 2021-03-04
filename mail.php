1d931f7848c13f3e8bdb50fdc879972ed04da631
dk2462000 <43301847+dk2462000@users.noreply.github.com>
2021-03-04 23:02:04 +0530

    FINAL WEBSITE

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+$to = 'bvmtpc2022@gmail.com';
+$subject = $_POST['subject'];
+$message = $_POST['message']; 
+$message.="  " ;
+$message.=$_POST['name'];
+
+$from = $_POST['email'];
+ 
+// Sending email
+if(mail($to, $subject, $message)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
+?>
